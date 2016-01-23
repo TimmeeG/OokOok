47a96e81d9f7930f878c79ce04b898e9990df27d
Tim Gallo <tgallo@truefit.io>
2016-01-23 09:08:41 -0500

    Initial Commit

---
+++ OokOok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OokOok
+//
+//  Created by Tim Gallo on 1/23/16.
+//  Copyright © 2016 Tim Gallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
